4b34b9ddbe003b1c4c21d78e3565a183e736310c
Emilio Dalla Torre <info@emiliodallatorre.it>
2023-06-14 11:39:40 +0200

    Costruito lo script che addestra il modello e lo salva.

---
+++ create_model.py
@@ -0,0 +1,44 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from pandas import DataFrame, read_csv
+
+
+# File to read the data and create a model for the WCDB dataset
+def load_data(path: str) -> DataFrame:
+    return read_csv(path,
+                    names=[
+                        'ID', 'Diagnosis', 'Radius', 'Texture', 'Perimeter', 'Area', 'Smoothness', 'Compactness',
+                        'Concavity', 'Concave Points', 'Symmetry', 'Fractal Dimension', 'Radius SE', 'Texture SE',
+                        'Perimeter SE', 'Area SE', 'Smoothness SE', 'Compactness SE', 'Concavity SE',
+                        'Concave Points SE', 'Symmetry SE', 'Fractal Dimension SE', 'Radius W', 'Texture W',
+                        'Perimeter W', 'Area W', 'Smoothness W', 'Compactness W', 'Concavity W', 'Concave Points W',
+                        'Symmetry W', 'Fractal Dimension W'],
+                    )
+
+
+def split_data(x: DataFrame, y: DataFrame) -> (DataFrame, DataFrame, DataFrame, DataFrame):
+    return train_test_split(x, y, test_size=0.2, random_state=42)
+
+
+def create_model(save_path: str) -> str:
+    data: DataFrame = load_data("data/data.csv")
+
+    full_x = data.drop(['ID', 'Diagnosis'], axis=1)
+    full_y = data['Diagnosis']
+
+    x_train, x_test, y_train, y_test = split_data(full_x, full_y)
+    model = RandomForestClassifier(n_estimators=100, random_state=42)  # Initialize the Random Forest classifier
+    model.fit(x_train, y_train)  # Train the model
+
+    y_pred = model.predict(x_test)  # Predict the test data
+    print("Accuracy: ", accuracy_score(y_test, y_pred))  # Print the accuracy of the model
+
+    # Save the model
+    from joblib import dump
+    dump(model, save_path)
+
+    return save_path
+
+
+create_model("output/random_forest_model.joblib")
